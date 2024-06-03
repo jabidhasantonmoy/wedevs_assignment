6781b5302b12e39bcb8be80dfd29af83723c7ba8
jabidhasantonmoy <jabidhasan1998@gmail.com>
2024-06-03 18:45:58 +0600

    Login page added, appcolor class added.

---
+++ lib/controller/app_theme/app_color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const backgroundColor = Color(0xFFF8F8F8);
+  static const cardColor = Color(0xFFFFFFFF);
+
+  // button color
+}
--- lib/view/auth/pages/login_page.dart
+++ lib/view/auth/pages/login_page.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(15, 15, 15, 15),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const SizedBox(height: 70),
+                        Image.asset(
+                          'asset/logo/logo_1.png',
+                          height: 50,
+                          width: 165,
+                        ),
+                        const SizedBox(height: 65),
+                        Text(
+                          'Sign In',
+                          style: GoogleFonts.roboto(
+                            fontSize: 25,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
